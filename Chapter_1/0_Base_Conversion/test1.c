80aaa8905ae4ad75358ddec9ae50f0fa9060b1f5
gipsyfine <gipsyfine@gmail.com>
2023-03-14 13:49:26 +0800

    添加bin文件夹，用于存放编译后的可执行文件

--- Chapter_1/0_Base_Conversion/test1.c
+++ Chapter_1/0_Base_Conversion/test1.c
@@ -2,5 +2,10 @@
 int main(void)
 {
     int a = 12,b = 012,c = 0x12;
-    printf("a = %d,b = %d,c = %d",a,b,c);
+    printf("a = %d,b = %d,c = %d\n",a,b,c);
+    printf("a = %d,b = %o,c = %x\n",a,b,c);
+    return 0;
 }
+
+// 十进制以数字1~9开头，八进制以数字0开头，十六进制以数字0x(或0X)开头
+// 二进制不能在C语言中直接书写
